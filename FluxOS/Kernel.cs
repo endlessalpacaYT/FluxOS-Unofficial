53d812d2aabd1f94e680c0bc448963c82221b8b8
ObsessedTech <90353497+endlessalpacaYT@users.noreply.github.com>
2024-04-01 16:41:03 +0100

    First Commit
    Dont Use this rn its got one function and it is just the base i will update this

---
+++ FluxOS/Kernel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace FluxOS
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine("FluxOS booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
