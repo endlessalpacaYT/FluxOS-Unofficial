ce49ce24504ab9e4603c9d911d795f98b8e09d95
ObsessedTech <90353497+endlessalpacaYT@users.noreply.github.com>
2024-04-01 20:41:51 +0100

    Add files via upload

--- FluxOS/Kernel.cs
+++ FluxOS/Kernel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Text;
 using Sys = Cosmos.System;
 
@@ -11,15 +12,55 @@ namespace FluxOS
         protected override void BeforeRun()
         {
             Console.Clear();
-            Console.WriteLine("FluxOS booted successfully. Type a line of text to get it echoed back.");
+            Console.WriteLine("FluxOS 0.01 booted successfully.");
+            Console.WriteLine();
+            Console.WriteLine("To Show A list Of Commands Type The Command 'Help'");
+            Console.WriteLine();
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
+            Console.Write("-> ");
             var input = Console.ReadLine();
-            Console.Write("Text typed: ");
+            if (input == "help" || input == "Help")
+            {
+                Help(); 
+            }
+            else if (input == "clear")
+            {
+                Console.Clear();
+            }
+            else if (input == "echo")
+            {
+                Echo();
+            }
+            else
+            {
+                Console.WriteLine("The command " + input + " is not a valid command");
+                Console.WriteLine("please make sure you are using the correct capitalization");
+                Console.WriteLine("do the command 'help' to see all valid commands");
+                Console.WriteLine();
+            }
+        }
+
+        public static void Help()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Here Are A List Of Commands You Can Perform:");
+            Console.WriteLine();
+            Console.WriteLine("help: shows a list of commands you can perform.");
+            Console.WriteLine("clear: Clears the console.");
+            Console.WriteLine("echo: Prints what you typed into the console.");
+            Console.WriteLine();
+        }
+
+        public static void Echo()
+        {
+            Console.WriteLine("Type what you would like to have printed to the console.");
+            Console.Write("echo: ");
+            string input = Console.ReadLine();
             Console.WriteLine(input);
+            Console.WriteLine();
         }
     }
 }
